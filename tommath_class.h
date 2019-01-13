edaac3a63d3ef612ffbc73562460f1cfd423eb97
openvela-robot <openvela-robot@xiaomi.com>
2019-01-13 20:21:36 +0800

    implemented Steffen Jaeckels superior suggestion regarding MoarVM's problem

--- tommath_class.h
+++ tommath_class.h
@@ -1,15 +1,3 @@
-/* LibTomMath, multiple-precision integer library -- Tom St Denis
- *
- * LibTomMath is a library that provides multiple-precision
- * integer arithmetic as well as number theoretic functionality.
- *
- * The library was designed directly after the MPI library by
- * Michael Fromberger but has been written from scratch with
- * additional optimizations in place.
- *
- * SPDX-License-Identifier: Unlicense
- */
-
 #if !(defined(LTM1) && defined(LTM2) && defined(LTM3))
 #if defined(LTM2)
 #   define LTM3
@@ -828,6 +816,7 @@
 #endif
 
 #if defined(BN_MP_RAND_C)
+#   define BN_MP_RAND_DIGIT_C
 #   define BN_MP_ZERO_C
 #   define BN_MP_ADD_D_C
 #   define BN_MP_LSHD_C
@@ -1218,7 +1207,3 @@
 #else
 #   define LTM_LAST
 #endif
-
-/* ref:         $Format:%D$ */
-/* git commit:  $Format:%H$ */
-/* commit time: $Format:%ai$ */
